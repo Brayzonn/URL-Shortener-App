e80cd6f066eb34e7a4cf96d8b4eeab12fb3066c4
Brayzonn <brayzonn@gmail.com>
2023-11-19 13:31:51 +0100

    proxy/baseURL changes

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -223,7 +223,7 @@ module.exports.freeSubmitUrl = async (req, res, next) => {
         if(freeLinkModel.length <= 2){
             // generate short url 
             const urlCode = shortid.generate();
-            const shortUrl = baseUrl + '/a/' + urlCode;
+            const shortUrl = baseUrl + '/b/' + urlCode;
 
             //cfreate and save new data
             const newurl = new freelinkModel({
@@ -258,6 +258,8 @@ function extractBaseUrl(fullUrl) {
 module.exports.getuRLnonUser = async (req, res, next) => {
 
     const linkCode = req.params.urlcode;
+
+    console.log(linkCode)
 
     try {
         const linkData = await freelinkModel.findOne({ urlCode: linkCode });
@@ -388,7 +390,7 @@ module.exports.submitUrl = async (req, res, next) => {
         } else {
             // generate short url 
             const urlCode = shortid.generate();
-            const shortUrl = baseUrl + '/b/' + urlCode;
+            const shortUrl = baseUrl + '/a/' + urlCode;
 
             //get user id and email
             const userInfo = await User.findOne({_id: req.id})
