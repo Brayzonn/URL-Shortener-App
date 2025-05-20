3a1ac71bc3bd17ecbfa83b0225636d2d4a3f8c49
Brayzonn <brayzonn@gmail.com>
2025-05-20 13:07:42 +0100

    cookie path restrictions removal check

--- server/app.js
+++ server/app.js
@@ -72,11 +72,10 @@ app.use(session({
         autoRemoveInterval: 60 
     }),
     cookie: {
-        secure: true, 
+        secure: true,
         httpOnly: true,
         sameSite: 'none' , 
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        path: '/linkly'
     }
 }));
 
